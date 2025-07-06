3cb9ef71df80345b035c592bafa689884c59fdec
Pratheep kanati <pratheepchowdhary@gmail.com>
2025-07-06 19:30:16 +0530

    Initial Commit

--- src/main/kotlin/com/connecthid/intellij/ui/filemanager/sftp/SftpSeTabProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.connecthid.intellij.ui.filemanager.sftp
-import com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributor
-import com.intellij.ide.actions.searcheverywhere.TabsCustomizationStrategy
-
-class SftpSeTabProvider: TabsCustomizationStrategy {
-    override fun getSeparateTabContributors(contributors: List<SearchEverywhereContributor<*>>): List<SearchEverywhereContributor<*>> {
-        return contributors.filter { it is SftpFileContributor }
-    }
-}
